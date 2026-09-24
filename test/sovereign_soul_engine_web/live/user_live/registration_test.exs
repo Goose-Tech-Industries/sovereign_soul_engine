d43ddabc9c54ef01ad032a4ce279608ea891b72f
Goose <gooserob@gmail.com>
2026-09-24 06:56:47 -0400

    Stabilize LiveView authentication coverage

--- test/sovereign_soul_engine_web/live/user_live/registration_test.exs
+++ test/sovereign_soul_engine_web/live/user_live/registration_test.exs
@@ -1,5 +1,5 @@
 defmodule SovereignSoulEngineWeb.UserLive.RegistrationTest do
-  use SovereignSoulEngineWeb.ConnCase, async: true
+  use SovereignSoulEngineWeb.ConnCase, async: false
 
   import Phoenix.LiveViewTest
   import SovereignSoulEngine.AccountsFixtures
