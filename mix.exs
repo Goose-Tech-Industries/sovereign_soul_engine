4cea34ba2308548e26ba375ab35221d41a04b4b6
Goose <gooserob@gmail.com>
2026-09-25 03:14:18 -0400

    Add deterministic coverage alias

--- mix.exs
+++ mix.exs
@@ -81,6 +81,7 @@ defmodule SovereignSoulEngine.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      coverage: ["compile", "test --cover test --no-compile"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
